0403cd0030922d27c3bd831a71d8e31ee239efd2
Jay Rawal <33748651+jayr1305@users.noreply.github.com>
2020-05-20 03:46:03 +0530

    weights usage

--- Backend/app.py
+++ Backend/app.py
@@ -1,3 +1,4 @@
+import csv
 import pickle 
 from flask_cors import CORS
 from flask import Flask,jsonify,request
@@ -11,6 +12,28 @@ app.config["DEBUG"] = True
 #=============================================================================================#
 
 
+def getData():
+    listy = {}
+    in_file = "../present/weighted.csv"  
+    row_reader = csv.reader(open(in_file, "r",encoding="utf8"))
+    c=0
+    for row in row_reader:
+        if(c==0):
+            print(row)
+        else:
+            if row[1] not in listy:
+            	listy[row[1]] = {}
+            
+            listy[row[1]][row[2]] = row[3]
+
+        c+=1
+        
+    print("\nTotal number : ",len(listy.keys()))
+    return listy
+
+weighted = getData()
+
+
 @app.route('/')
 def home():
 	return "<h1>This is the backend for our PSOSM Project</h1>"
@@ -22,12 +45,14 @@ def getMyDetails(primary_friend):
 	with open("../graphs/friend_graph_"+primary_friend+".pickle", 'rb') as f:
 		graph_list = pickle.load(f)
 	
-	try: 
+	try:
 		results = list(graph_list.keys())
+		# print(len(results))
+
 	except Exception as E:
 		results = {'Error': str(E)}
 
-	return jsonify(results)
+	return jsonify(weighted)
 
 
 @app.route('/<primary_friend>/<listed_friend>')
